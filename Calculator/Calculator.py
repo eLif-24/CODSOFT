a121d3b519d61d4024b7ca6afebe4731a622e6e3
eLif-24 <kalranaman3@gmail.com>
2024-08-18 22:09:12 +0530

    Python Project

---
+++ Calculator/Calculator.py
@@ -0,0 +1,51 @@
+x,y=0,0
+def add():
+    x,y=int(input("Enter the First number:")),int(input("Enter the Second number:"))
+    c=x+y
+    print(f"The Summation of the two number is {c}")
+    print("\n")
+
+def sub():
+    x,y=int(input("Enter the First number:")),int(input("Enter the Second number:"))
+    c=x-y
+    print(f"The Difference of the two number is {c}")
+    print("\n")
+
+def multi():
+    x,y=int(input("Enter the First number:")),int(input("Enter the Second number:"))
+    c=x*y
+    print(f"The Multiplication of the two number is {c}")
+    print("\n")
+def div():
+    x,y=int(input("Enter the First number:")),int(input("Enter the Second number:"))
+    c=x/y
+    print(f"The Division of the two number is {c}")
+    print("\n")
+if __name__=="__main__" :
+    print("Hii>>\nWelcome to the N.K. Calculator!!")
+    print("\n")
+    
+    while True:
+        print("What would you like to do today??")
+        print("---------------------------------")
+        print("1. Addition.")
+        print("2. Subtraction.")
+        print("3. Multiplication.")
+        print("4. Division.")
+        print("5. Exit the application.")
+        print("\n")
+        n=int(input("Please select from 1-4:"))
+        
+        if n==1:
+            add()
+        elif n==2:
+            sub()
+        elif n==3:
+            multi()
+        elif n==4:
+            div()
+        elif n==5:
+            print("GoodBye. Have a Great Day!!")
+            break
+        else:
+            print("Invalid Input. Please try again")
