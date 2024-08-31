25e2cbf919a53837b1bb80333892cc02c6d58434
eLif-24 <kalranaman3@gmail.com>
2024-08-31 21:33:21 +0530

    Python Project

--- Rock Paper Scissor Game/rps.py
+++ Rock Paper Scissor Game/rps.py
@@ -1,74 +1,43 @@
 #Rock Paper Scissors Game
 import random
 
-comp=0
-user=0
-
-z=int()
+options=[1,2,3]
+options_1={1:"Rock",2:"Paper",3:"Scissors"}
 def rps_comp():
-    y=[1,2,3]
-    z=random.choice(y)
+    z=random.choice(options_1)
     return z
-def game(x,y,comp,user):
-    comp,user=0,0
-    if x==1 and y==1:
-        print("This is a tie\n")
-    elif x==1 and y==2:
-        print("Comp wins\n")
-        comp=+1
-    elif x==1 and y==3:
-        print("You win\n")
-        user=+1
-    elif x==2 and y==1:
-        print("You win\n")
-        user=+1
-    elif x==2 and y==2:
-        print("This is a tie\n")
-    elif x==2 and y==3:
-        print("Comp wins\n")
-        comp=+1
-    elif x==3 and y==1:
-        print("Comp wins\n")
-        comp=+1
-    elif x==3 and y==2:
-        print("You win\n")
-        user=+1
-    elif x==3 and y==3:
-        print("This is a tie\n")
+def game(k,y):
+    if k==y:
+        print("It's a Tie!")
+    elif k=='Rock' and y=='Scissors':
+        print("You win!")
+    elif k=='Paper' and y=='Rock':
+        print("You win!")
+    elif k=='Scissors' and y=='Paper':
+        print("You win!")
     else:
-        print("Invalid choice")
+        print("You Lose")
     
-
     
     
 print("Welcome to Rock Paper and Scissors!!")
 print("------------------------------------")
-
-
-while True:
-    print("What would you like to choose?")
-    print("1.Rock\n2.Paper\n3.Scissors\n4.exit")
+running=True
+while running:  
+    x=None
     
-    x=int(input("Please enter the choice:\n"))
+    while x not in options:
+        print("What would you like to choose?")
+        print("1.Rock\n2.Paper\n3.Scissors\n")
+        x=int(input("Please enter the choice number:\n"))
+    k=options_1[x]
     y=rps_comp()
-    if x==1:
-        print("You choose Rock")
-    elif x==2:
-        print("You choose Paper")
-    elif x==3:
-        print("You choose scissors")
-    if x==1 or x==2 or x==3:
-        if y==1:
-            print("Computer choose Rock\n")
-        elif y==2:
-            print("Computer choose Paper\n")
-        elif y==3:
-            print("Computer choose scissors\n")
-    if x!=4:
-        game(x,y,comp,user)
-    elif x==4:
-        print("Thank you for playing!")
-        break
-    else:
-        print("Invalid choice\n")
-        break
+    print(f"You choose: {k}")
+    print(f"Computer choose: {y}")
+    game(k,y)
+    if not input("Do you want to play again: (Y/N)").upper()=="Y":
+        running=False
+print("Thanks for playing!!")
+
+    
+    
